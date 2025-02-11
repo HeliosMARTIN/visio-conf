320ed66c7f0739597cca210bb0b3af761bb4df26
Helios Martin <heliosmartin123@gmail.com>
2025-02-11 09:59:24 +0100

    add formats to list messages for other devs

--- ListeMessages.js
+++ ListeMessages.js
@@ -1,21 +1,13 @@
 /**
- * @fileoverview Fichier contenant les constantes des messages émis et reçus par le composant SocketIO
- * À savoir que les messages émis sont les messages envoyés par le client au serveur et les messages reçus sont les messages envoyés par le serveur au client
- *
- * @author Mathis LAMBERT
- * @module ListeMessages
- */
-
-/**
  * Liste des messages émis
  * @type {string[]}
  */
 const ListeMessagesEmis = [
-    "client_deconnexion",
-    "connexion_requete",
-    "inscription_requete",
-    "liste_utilisateurs_requete",
-    "messages_requete",
+    "client_deconnexion", // No data
+    "connexion_requete", // { login: string, mdp: string }
+    "inscription_requete", // { login: string, mdp: string, firstname: string, lastname: string, user_phone: string, user_job: string, user_desc: string }
+    "liste_utilisateurs_requete", // No data
+    "messages_requete", // No data
 ]
 
 /**
@@ -23,11 +15,11 @@ const ListeMessagesEmis = [
  * @type {string[]}
  */
 const ListeMessagesRecus = [
-    "client_deconnexion",
-    "connexion_reponse",
-    "inscription_reponse",
-    "liste_utilisateurs_reponse",
-    "messages_reponse",
+    "client_deconnexion", // No data
+    "connexion_reponse", // { etat: string, user?: { firstname: string, lastname: string, email: string } }
+    "inscription_reponse", // { etat: string, user?: { firstname: string, lastname: string, email: string } }
+    "liste_utilisateurs_reponse", // { etat: boolean, users?: User[], error?: string }
+    "messages_reponse", // { etat: boolean, messages?: Message[], error?: string }
 ]
 
 module.exports = {
