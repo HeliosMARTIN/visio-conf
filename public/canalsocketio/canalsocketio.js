ffbd5a6433d9dce824b24cc1a4bd7f18b35cc4ff
Helios Martin <heliosmartin123@gmail.com>
2025-02-11 09:12:42 +0100

    re add files

--- public/canalsocketio/canalsocketio.js
+++ public/canalsocketio/canalsocketio.js
@@ -1,44 +1,47 @@
-class CanalSocketio{
+class CanalSocketio {
+    controleur
+    nomDInstance
+    socket
 
-	controleur;
-	nomDInstance;
-	socket;
-	
-	
-	listeDesMessagesEmis;
-	listeDesMessagesRecus;
-	verbose=false;	
-	
-	constructor(s,c,nom){
-		
-		this.controleur=c;			
-		
-		this.nomDInstance=nom;
-		
-		this.socket = s('http://localhost:3220', { });
-		
-		this.socket.on("message", (msg) => {
-			if(this.controleur.verboseall || this.verbose) console.log("INFO ("+this.nomDInstance+"): reçoit ce message:"+msg);
-			this.controleur.envoie(this,JSON.parse(msg));
-		});
-		this.socket.on("donne_liste", (msg) => {
-			var listes=JSON.parse(msg);
-			this.listeDesMessagesEmis=listes.emission;
-			this.listeDesMessagesRecus=listes.abonnement;
-			if(this.controleur.verboseall || this.verbose) console.log("INFO ("+this.nomDInstance+"): inscription des messages de CanalSocketio");
-			this.controleur.inscription(this,listes.emission, listes.abonnement);
-		});
-		
-		
-		this.socket.emit("demande_liste",{});
-		
-		
-	
-		
-	}
-	
-	traitementMessage(mesg){
-		this.socket.emit("message",JSON.stringify(mesg));
-		
-	}
+    listeDesMessagesEmis
+    listeDesMessagesRecus
+    verbose = false
+
+    constructor(s, c, nom) {
+        this.controleur = c
+
+        this.nomDInstance = nom
+
+        this.socket = s("http://localhost:3220", {})
+
+        this.socket.on("message", (msg) => {
+            if (this.controleur.verboseall || this.verbose)
+                console.log(
+                    "INFO (" + this.nomDInstance + "): reçoit ce message:" + msg
+                )
+            this.controleur.envoie(this, JSON.parse(msg))
+        })
+        this.socket.on("donne_liste", (msg) => {
+            var listes = JSON.parse(msg)
+            this.listeDesMessagesEmis = listes.emission
+            this.listeDesMessagesRecus = listes.abonnement
+            if (this.controleur.verboseall || this.verbose)
+                console.log(
+                    "INFO (" +
+                        this.nomDInstance +
+                        "): inscription des messages de CanalSocketio"
+                )
+            this.controleur.inscription(
+                this,
+                listes.emission,
+                listes.abonnement
+            )
+        })
+
+        this.socket.emit("demande_liste", {})
+    }
+
+    traitementMessage(mesg) {
+        this.socket.emit("message", JSON.stringify(mesg))
+    }
 }
