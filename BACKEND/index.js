a53ffa8ca1561e4c44d05d5bf345d2279ab65cdb
Helios Martin <heliosmartin123@gmail.com>
2025-02-13 08:53:02 +0100

    add env.example and use env variables in index

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -19,7 +19,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-const port = 3220
+const port = process.env.PORT || 3220
 const server = createServer(app)
 const io = new Server(server, { cors: { origin: "*" } })
 
@@ -30,36 +30,20 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json())
 
-app.use((req, res, next) => {
-    const token = req.headers["authorization"]
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) {
-                return res.status(401).json({ message: "Unauthorized" })
-            } else {
-                req.user = decoded
-                next()
-            }
-        })
-    } else {
-        next()
-    }
-})
-
-var verbose = true
+var verbose = process.env.VERBOSE === "true"
 var controleur = new Controleur()
-controleur.verboseall = true
+controleur.verboseall = verbose
 
 // Instanciation des services pour les initialiser et déclencher leur enregistrement auprès du contrôleur
 const usersService = new UsersService(controleur, "UsersService")
 const messagesService = new MessagesService(controleur, "MessagesService")
-
 const canalsocketio = new CanalSocketio(io, controleur, "canalsocketio")
 
 main().catch((err) => console.log(err))
 
 async function main() {
-    await mongoose.connect(
-        "mongodb://root:root@127.0.0.1:27017/visio-conf?authSource=admin"
-    )
+    await mongoose.connect(process.env.MONGO_URI, {
+        user: process.env.MONGO_USER,
+        pass: process.env.MONGO_PASSWORD,
+    })
 }
