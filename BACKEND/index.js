0c6197b81a80d1546d95fd5a05ff221aaa83cb0d
Alexandre <alexandre.testa.at@gmail.com>
2025-02-13 09:16:23 +0100

    merge

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -28,21 +28,22 @@ server.listen(port, () => {
 })
 app.use(cors())
 app.use(express.static(path.join(__dirname, "public")))
+app.use(express.json())
 
-var verbose = true
+var verbose = process.env.VERBOSE === "true"
 var controleur = new Controleur()
-controleur.verboseall = true
+controleur.verboseall = verbose
 
-var canalsocketio = new CanalSocketio(io, controleur, "canalsocketio")
-var gestionUtilisateur = new GestionUtilisateur(
-    controleur,
-    "gestion_utilisateur"
-)
+// Instanciation des services pour les initialiser et déclencher leur enregistrement auprès du contrôleur
+const usersService = new UsersService(controleur, "UsersService")
+const messagesService = new MessagesService(controleur, "MessagesService")
+const canalsocketio = new CanalSocketio(io, controleur, "canalsocketio")
 
     main().catch((err) => console.log(err))
 
 async function main() {
-    await mongoose.connect(
-        "mongodb://root:root@127.0.0.1:27017/visio-conf?authSource=admin"
-    )
+    await mongoose.connect(process.env.MONGO_URI, {
+        user: process.env.MONGO_USER,
+        pass: process.env.MONGO_PASSWORD,
+    })
 }
