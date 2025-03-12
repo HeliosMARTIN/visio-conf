ae7ddc8bd35f2b2dce2711d1c21135dfa3ceef53
Helios Martin <heliosmartin123@gmail.com>
2025-03-12 15:37:16 +0100

    fix app context redirection

--- FRONTEND/context/AppContext.tsx
+++ FRONTEND/context/AppContext.tsx
@@ -51,7 +51,7 @@ export const AppContextProvider = ({
     }, [])
 
     useEffect(() => {
-        if (!(currentUser || !token)) {
+        if (!token) {
             setCurrentUser(null)
             router.push("/login")
         }
