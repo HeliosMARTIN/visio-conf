e4e748f815317f3d16554484ef5e526bdda73a9c
Helios Martin <heliosmartin123@gmail.com>
2025-04-04 16:10:13 +0200

    Merge branch 'master' of https://github.com/HeliosMARTIN/visio-conf

--- FRONTEND/components/Menu.tsx
+++ FRONTEND/components/Menu.tsx
@@ -1,94 +1,145 @@
 "use client"
+
+import { useState } from "react"
 import Image from "next/image"
+import Link from "next/link"
 import styles from "./Menu.module.css"
 import { useAppContext } from "@/context/AppContext"
-import Link from "next/link"
 
-export default function Menu() {
+export default function Menu({ children }: { children: React.ReactNode }) {
     const { currentUser } = useAppContext()
+    const [collapsed, setCollapsed] = useState(true)
 
     return (
-        <section className={styles.section}>
-            <div className={styles.menu}>
-                <div>
-                    <Link href="/" className={styles.logo}>
-                        <Image
-                            className={styles.logoImage}
-                            src="/logo_Univ.png"
-                            alt="Logo"
-                            width={40}
-                            height={40}
-                            priority
-                        />
-                        <h2 className={styles.menuText}>
-                            Université de Toulon
-                        </h2>
-                    </Link>
-                </div>
-                <div>
-                    <ul className={styles.allIcones}>
-                        <Link href="/discussions" className={styles.link}>
+        <div className={styles.mainLayout}>
+            <div
+                className={`${styles.sidebar} ${
+                    collapsed ? styles.collapsed : styles.expanded
+                }`}
+                onMouseEnter={() => setCollapsed(false)}
+                onMouseLeave={() => setCollapsed(true)}
+            >
+                <div className={styles.sidebarContent}>
+                    <div className={styles.logoContainer}>
+                        <Link href="/" className={styles.logoLink}>
                             <Image
-                                src="/conversation.svg"
-                                alt="Icone Conversation"
-                                width={25}
-                                height={25}
+                                src="/logo_Univ.png"
+                                alt="Logo"
+                                width={40}
+                                height={40}
                                 priority
+                                className={styles.logoImage}
                             />
-                            <h2 className={styles.menuText}>Discussions</h2>
+                            <h2
+                                className={`${styles.menuText} ${
+                                    collapsed ? styles.hidden : ""
+                                }`}
+                            >
+                                Université de Toulon
+                            </h2>
                         </Link>
-                        <Link href="/utilisateurs" className={styles.link}>
+                    </div>
+
+                    <nav className={styles.menuItems}>
+                        <Link href="/discussions" className={styles.menuItem}>
+                            <div className={styles.menuIcon}>
+                                <Image
+                                    src="/conversation.svg"
+                                    alt="Discussions"
+                                    width={25}
+                                    height={25}
+                                />
+                            </div>
+                            <span
+                                className={`${styles.menuItemText} ${
+                                    collapsed ? styles.hidden : ""
+                                }`}
+                            >
+                                Discussions
+                            </span>
+                        </Link>
+
+                        <Link href="/equipes" className={styles.menuItem}>
+                            <div className={styles.menuIcon}>
+                                <Image
+                                    src="/users.svg"
+                                    alt="equipes"
+                                    width={25}
+                                    height={25}
+                                />
+                            </div>
+                            <span
+                                className={`${styles.menuItemText} ${
+                                    collapsed ? styles.hidden : ""
+                                }`}
+                            >
+                                Equipes
+                            </span>
+                        </Link>
+
+                        <Link href="/dossiers" className={styles.menuItem}>
+                            <div className={styles.menuIcon}>
+                                <Image
+                                    src="/folder.svg"
+                                    alt="Dossiers"
+                                    width={25}
+                                    height={25}
+                                />
+                            </div>
+                            <span
+                                className={`${styles.menuItemText} ${
+                                    collapsed ? styles.hidden : ""
+                                }`}
+                            >
+                                Dossiers
+                            </span>
+                        </Link>
+
+                        <Link href="/annuaire" className={styles.menuItem}>
+                            <div className={styles.menuIcon}>
+                                <Image
+                                    src="/livre.svg"
+                                    alt="Annuaire"
+                                    width={25}
+                                    height={25}
+                                />
+                            </div>
+                            <span
+                                className={`${styles.menuItemText} ${
+                                    collapsed ? styles.hidden : ""
+                                }`}
+                            >
+                                Annuaire
+                            </span>
+                        </Link>
+                    </nav>
+
+                    <div className={styles.profileContainer}>
+                        <Link href="/profil" className={styles.profileLink}>
                             <Image
-                                src="/users.svg"
-                                alt="Icone User"
-                                width={25}
-                                height={25}
+                                className={styles.profileImage}
+                                src={
+                                    currentUser?.picture
+                                        ? `https://visioconfbucket.s3.eu-north-1.amazonaws.com/${currentUser.picture}`
+                                        : `https://visioconfbucket.s3.eu-north-1.amazonaws.com/default_profile_picture.png`
+                                }
+                                alt="profil"
+                                width={40}
+                                height={40}
                                 priority
+                                unoptimized
                             />
-                            <h2 className={styles.menuText}>Utilisateurs</h2>
                         </Link>
-                        <Link href="/files" className={styles.link}>
-                            <Image
-                                src="/folder.svg"
-                                alt="Icone dossier"
-                                width={25}
-                                height={25}
-                                priority
-                            />
-                            <h2 className={styles.menuText}>Drive</h2>
-                        </Link>
-                        <Link href="/annuaire" className={styles.link}>
-                            <Image
-                                src="/livre.svg"
-                                alt="Icone livre"
-                                width={25}
-                                height={25}
-                                priority
-                            />
-                            <h2 className={styles.menuText}>Annuaire</h2>
-                        </Link>
-                    </ul>
+                    </div>
                 </div>
             </div>
-            <div>
-                <div className={styles.profil}>
-                    <Link href="/profil">
-                        <Image
-                            className={styles.profil}
-                            src={
-                                currentUser?.picture
-                                    ? `https://visioconfbucket.s3.eu-north-1.amazonaws.com/${currentUser.picture}`
-                                    : `https://visioconfbucket.s3.eu-north-1.amazonaws.com/default_profile_picture.png`
-                            }
-                            alt="profil"
-                            width={40}
-                            height={40}
-                            priority
-                            unoptimized
-                        />
-                    </Link>
-                </div>
+            <div
+                className={`${styles.contentContainer} ${
+                    !collapsed ? styles.contentExpanded : ""
+                }`}
+            >
+                {children}
             </div>
-        </section>
+        </div>
     )
 }
